e83ed0ff1e40aaff24e9f9757ed5bf2c0cad5bbc
Thailand IOT Sensor <37290558+sensorthai@users.noreply.github.com>
2023-06-08 10:19:18 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,4 +1,4 @@
-Blockly.Python['block_type'] = function(block) {
+Blockly.Python['beservo'] = function(block) {
   var dropdown_name = block.getFieldValue('NAME');
   var value_code = Blockly.Python.valueToCode(block, 'code', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
