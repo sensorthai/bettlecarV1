b6683ec3bb7e77c92ca83524109768afe9f5a76c
Thailand IOT Sensor <37290558+sensorthai@users.noreply.github.com>
2023-06-27 17:15:44 +0700

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,27 +1,172 @@
 ({
-    name: "Bettle Car V1", // Category Name
-    description: "Bettle CarV1",
-    author: "up-skill.asia",
-    category: "Device Control",
-    version: "1.1.0",
-    icon: "/static/icon.png", // Category icon
-    color: "#0271D9", // Category color (recommend some blocks color)
-    blocks: [ // Blocks in Category
-        {
+    name: "NETPIE",
+    description: "Connect your device to  IoT platform",
+    author: "Chavee Issariyapat",
+    category: "Communication",
+    version: "1.0.4",
+    icon: "/static/icon.png",
+    color: "#4A7CCC",
+    blocks: [
+       {
             xml: `
-                <block type="b_servo">
-                    <value name="pin">
-                        <shadow type="math_number">
-                            <field name="NUM">14</field>
+                <block type="netpie_connect">
+                    <value name="device_id">
+                        <shadow type="text">
+                            <field name="TEXT">00000000-0000-0000-0000-000000000000</field>
                         </shadow>
                     </value>
-                    <value name="angle">
-                        <shadow type="math_number">
-                            <field name="NUM">90</field>
+                    <value name="device_token">
+                        <shadow type="text">
+                            <field name="TEXT">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</field>
                         </shadow>
                     </value>
                 </block>
             `
+        },
+        {
+            xml: `
+                <block type="netpie_on_connected">
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_on_disconnected">
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_publish">
+                    <value name="topic">
+                        <shadow type="text">
+                            <field name="TEXT"></field>
+                        </shadow>
+                    </value>
+                    <value name="payload">
+                        <shadow type="text">
+                            <field name="TEXT">ON</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_subscribe">
+                    <value name="topic">
+                        <shadow type="text">
+                            <field name="TEXT"></field>
+                        </shadow>
+                    </value>
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_on_reveived_msg">
+                    <value name="topic">
+                        <shadow type="text">
+                            <field name="TEXT"></field>
+                        </shadow>
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_msg_payload">
+                    <value name="datatype">
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_write_shadow_field">
+                    <value name="field">
+                        <shadow type="text">
+                            <field name="TEXT">home.bedroom.temp</field>
+                        </shadow>
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_read_shadow">
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_on_shadow_updated">
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_shadow_field">
+                    <value name="field">
+                        <shadow type="text">
+                            <field name="TEXT">home.bedroom.temp</field>
+                        </shadow>
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_on_reveived_private_msg">
+                    <value name="topic">
+                        <shadow type="text">
+                            <field name="TEXT"></field>
+                        </shadow>
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_private_msg_payload">
+                    <value name="datatype">
+                    </value>
+
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_text">
+                    <value name="value">
+                    </value>
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_number">
+                    <value name="value">
+                    </value>
+                </block>
+            `
+        },
+        {
+            xml: `
+                <block type="netpie_boolean">
+                    <value name="value">
+                    </value>
+                </block>
+            `
         }
+    ],
+    chip: [
+        "ESP32",
+        "RP2-WiFi"
     ]
 });
